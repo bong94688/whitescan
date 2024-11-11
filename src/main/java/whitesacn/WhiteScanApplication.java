351a1e765cac914bbfe98164f64014af2e668df9
심봉교 <bonggyo.sim@ipageon.com>
2024-11-11 14:57:48 +0900

    init : greale 프로젝트 초기 커밋 java17 , springboot 3.3.0

---
+++ src/main/java/whitesacn/WhiteScanApplication.java
@@ -0,0 +1,13 @@
+package whitesacn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteScanApplication.class, args);
+    }
+
+}
